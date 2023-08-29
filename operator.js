5c34abcae14b0696772ea8a6e53491b7fb3e3b7c
Inventus-s <handle.extras@gmail.com>
2023-08-29 22:56:50 +0530

    operators

---
+++ operator.js
@@ -0,0 +1,24 @@
+// output of 3**3
+console.log(3**3); //power
+
+// Add number and String
+console.log(4+'Mukund');
+
+// Swap two number 
+let a = 15;
+let b = 10;
+let c = a;
+a = b;
+b = c;
+console.log(`Swap value of a : ${a} and b : ${b}`);
+
+// Swap two number without 3rd variable
+let x,y;
+x = 1;
+y = 2;
+
+x = x + y;
+y = x - y;
+x = x - y;
+
+console.log(`Swap value of x : ${x} and y : ${y}`);
