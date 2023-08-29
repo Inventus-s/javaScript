f65dac5625df293ef314e82043e0b1893eb3d396
Inventus-s <handle.extras@gmail.com>
2023-08-29 22:15:46 +0530

    Data Tpes

--- dataTypes.js
+++ dataTypes.js
@@ -1,12 +1,21 @@
 // Practice Questions
 
-console.log(10 + "20");
-console.log(9-"5"); // bug
-console.log("Java"+"Script");
-console.log(""+"");
-console.log(""+0);
-console.log("mukund"-"kumar");
-console.log(true + true);
-console.log(true + false);
-console.log(false + true);
-console.log(false - true);
+// console.log(10 + "20");
+// console.log(9-"5"); // bug
+// console.log("Java"+"Script");
+// console.log(""+"");
+// console.log(""+0);
+// console.log("mukund"-"kumar");
+// console.log(true + true);
+// console.log(true + false);
+// console.log(false + true);
+// console.log(false - true);
+
+
+// NaN Practice
+
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN(NaN));
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
