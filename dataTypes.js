ac0018a74e437f75a449eca7149fb757f64c329f
Inventus-s <handle.extras@gmail.com>
2023-08-29 21:14:45 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+// Practice Questions
+
+console.log(10 + "20");
+console.log(9-"5"); // bug
+console.log("Java"+"Script");
+console.log(""+"");
+console.log(""+0);
+console.log("mukund"-"kumar");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
