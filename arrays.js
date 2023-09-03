1caec3ef5e6caab9902913c5a0ee457cfdbbbbcd
Inventus-s <handle.extras@gmail.com>
2023-09-03 21:02:41 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// Arrays
+
+const fruits = ['apple','mango','orange','melon','banana'];
+
+console.log(fruits[2]); // Orange
+
+console.log(fruits.length); // 5
+
+console.log(fruits[fruits.length - 1]); // banana
