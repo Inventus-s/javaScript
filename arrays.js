f912a5d98a0e7eddbdb5bd5c8a29c16c23973c72
Inventus-s <handle.extras@gmail.com>
2023-09-04 23:30:26 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -7,3 +7,10 @@ console.log(fruits[2]); // Orange
 console.log(fruits.length); // 5
 
 console.log(fruits[fruits.length - 1]); // banana
+
+// indexof()
+
+console.log(fruits.indexOf('melon')) ;
+
+const para = "My name is Mukund kumar";
+console.log(para.indexOf("m",10));
