3d421fe3aa60960d8bc1633652d87ceecc8cbe06
Inventus-s <handle.extras@gmail.com>
2023-09-15 11:31:14 +0530

    Map

---
+++ map.js
@@ -0,0 +1,19 @@
+// const arr = [1,4,9,16,25];
+
+// let newArr = arr.map((ele, i, ar)=>{
+//     return `Index no. = ${i} and the value is ${ele} belongs to ${ar}`
+// });
+
+// console.log(newArr);
+
+// Challenge Time
+
+// find Square Root
+/*let arr = [25,36,49,64,81];
+let arrSqr = arr.map((ele)=> Math.sqrt(ele));
+console.log(arrSqr);*/
+
+// multiply by 2 and return more than 10
+// let arr = [2,3,4,6,8];
+// let arr2 = arr.map((ele)=>ele*2).filter((ele)=>ele>10);
+// console.log(arr2);
