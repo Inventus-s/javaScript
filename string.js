ef6ae14866422a925711db502783b39a07e52267
Inventus-s <handle.extras@gmail.com>
2023-10-04 23:13:40 +0530

    strings

---
+++ string.js
@@ -0,0 +1,13 @@
+let myName = "Mukund Kumar"
+console.log(myName.length);
+
+// Indexof()
+let boiData = 'My name is Mukund Kumar'
+console.log(boiData.indexOf("name"));
+// lastIndeOf()
+console.log(boiData.lastIndexOf("n"));
+// search
+console.log(boiData.search('name'));
+
+// Slice
+console.log(myName.slice(0,5));
