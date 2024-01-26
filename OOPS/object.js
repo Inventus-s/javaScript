82d706659e8223d40d55dd88e7f687a0ce461f12
Inventus-s <handle.extras@gmail.com>
2024-01-26 14:17:15 +0530

    Object properties

--- OOPS/object.js
+++ OOPS/object.js
@@ -39,3 +39,20 @@ bioData = {
 
 
 console.log(bioData.myName.insta);
+
+// Object Properties --> Dynamic properties
+// let myName = "Mukund";
+// const Bio = {
+//     [myName] : 'Hello how are you?',
+//     [20+6] : 'is my age'
+// }
+// console.log(Bio);
+
+// no need to write key and value, if both are same
+let myName = 'Mukund Kumar';
+let myAge = 22;
+const myBio = {
+    myName,
+    myAge
+}
+console.log(myBio);
--- OOPS/this.js
+++ OOPS/this.js
@@ -5,18 +5,39 @@ console.log(this); // refers to window global object
 // }
 // myName();
 
-var myNames = 'mukund';
-function myName(){
-    console.log(this.myNames);
-}
-myName();
+// var myNames = 'mukund';
+// function myName(){
+//     console.log(this.myNames);
+// }
+// myName();
 
-const obj = {
-    myAge : 22,
-    myName(){
-        console.log(this.myAge);
-    }
-}
-obj.myName();
+// const obj = {
+//     myAge : 22,
+//     myName(){
+//         console.log(this.myAge);
+//     }
+// }
+// obj.myName();
 
 // this object not works with arrow function
+// const obj = {
+//     myAge : 26,
+//     myName : () => {
+//         console.log(this);
+//     }
+// }
+
+// obj.myName();
+
+let bioData = {
+    myName : {
+        realName : 'Mukund',
+        channelName : 'liberator'
+    },
+
+    myAge : 26,
+    getData() {
+        console.log(`My name is ${this.myName.realName} and my age is ${this.myAge}`);
+    }
+}
+bioData.getData();
