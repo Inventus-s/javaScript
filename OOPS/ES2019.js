a734e6b34fd610917b846ef4e6b9a3106415cd5d
Inventus-s <handle.extras@gmail.com>
2024-01-26 15:15:35 +0530

    ES2019

---
+++ OOPS/ES2019.js
@@ -0,0 +1,16 @@
+// Array.prototype.{flat,flatMap}
+
+const std = [
+    [1],[2],[3],[4,[5,6]]
+]
+
+// console.log(Object.fromEntries(std));
+
+console.log(std.flat());
+console.log(std.flat(2));
+console.log(std.flat(Infinity));
+
+// Object.fromEntries() --> come back from array to object
+const person = {name:'fred',age:26}
+let arr = Object.entries(person);
+console.log(Object.fromEntries(arr));
