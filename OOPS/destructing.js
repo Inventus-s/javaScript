8402eec11d903fa14c1d8943c0dab533a1622179
Inventus-s <handle.extras@gmail.com>
2024-01-26 14:03:44 +0530

    this

---
+++ OOPS/destructing.js
@@ -0,0 +1,23 @@
+// unpack array and object
+
+// Array Destructing
+// const bioData = ['mukund','kumar',22];
+// let myFname = bioData[0];
+// let myLname = bioData[1];
+// let myAge = bioData[2];
+// console.log(myAge);
+
+// with destructing
+// let [myFname,myLname,myAge] = bioData;
+// console.log(myAge);
+
+// we can add values too
+// let [myFname,myLname,myAge,myDegree = 'BCA'] = bioData;
+// console.log(myDegree);
+
+// Object Destructing
+ const bioData = {
+    myFname : 'Mukund',
+    myFname : 'Kumar',
+    myAge : 26
+ }
